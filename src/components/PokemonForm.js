d23613a0096358b55100944a333dac53400c0833
Joseph-Burks <joe_burks_3@yahoo.com>
2020-12-16 10:49:41 -0600

    solution

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -2,11 +2,32 @@ import React from 'react'
 import { Form } from 'semantic-ui-react'
 
 class PokemonForm extends React.Component {
+
+  state = {
+    name: '',
+    hp: '',
+    frontUrl: '',
+    backUrl: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    e.target.reset()
+    this.props.addPokemon(this.state)
+  }
+
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={() => {console.log("submitting form...")}}>
+        <Form onSubmit={this.handleSubmit} onChange={this.handleChange} >
           <Form.Group widths="equal">
             <Form.Input fluid label="Name" placeholder="Name" name="name" />
             <Form.Input fluid label="hp" placeholder="hp" name="hp" />
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,54 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    pokemon: [],
+    searchTerm: ''
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(pokemon => this.setState({pokemon}))
+  }
+
+  setSearchTerm = e => {
+    this.setState({searchTerm: e.target.value.toLowerCase()})
+  }
+
+  addPokemon = newPokemon => {
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: newPokemon.name,
+        hp: newPokemon.hp,
+        sprites: {
+          front: newPokemon.frontUrl,
+          back: newPokemon.backUrl
+        }
+      })
+    })
+    .then(res => res.json())
+    .then(pokemon => {
+      this.setState({pokemon: [...this.state.pokemon, pokemon]})
+    })
+  } 
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm addPokemon={this.addPokemon} />
         <br />
-        <Search />
+        <Search setSearchTerm={this.setSearchTerm} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.pokemon.filter(pokemon => pokemon.name.toLowerCase().includes(this.state.searchTerm))} />
       </Container>
     )
   }
